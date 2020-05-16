6a8fe334b23b4758e1f22b25b281d53270575f36
SaiKumarKadimisetty <SaiKumarKadimisetty@host.docker.internal>
2020-05-16 16:34:12 +0800

    test

---
+++ DBS-test/src/main/java/DBSTest/DBS_test/App.java
@@ -0,0 +1,15 @@
+package DBSTest.DBS_test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	
+    	System.out.println("welcome toworld");
+        System.out.println( "Hello World!" );
+    }
+}
